cad5aa59a444493f92d865607033b55c7a851577
חומוס גרגירים <134533421+restaurant2@users.noreply.github.com>
2025-11-02 17:50:27 +0200

    Update firebaseConfig.js

--- gift-tmfv/src/firebaseConfig.js
+++ gift-tmfv/src/firebaseConfig.js
@@ -1,12 +1,15 @@
-// src/firebaseConfig.js
-// ضع هنا إعدادات Firebase الخاصة بك قبل النشر.
-export const firebaseConfig = {
-  apiKey: 'AIzaSyCmL64vfBE43K9zg_7d5iqdIimHncbVz58',
-  authDomain: 'gift-tmfv.firebaseapp.com',
-  projectId: 'gift-tmfv',
-  storageBucket: 'gift-tmfv.firebasestorage.app',
-  messagingSenderId: '159663278849',
-  appId: '1:159663278849:web:5a70538d543c734e98e900',
-  measurementId: 'G-0XCYKDBFDV'
-}
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCmL64vfBE43K9zg_7d5iqdIimHncbVz58",
+  authDomain: "gift-tmfv.firebaseapp.com",
+  projectId: "gift-tmfv",
+  storageBucket: "gift-tmfv.firebasestorage.app",
+  messagingSenderId: "159663278849",
+  appId: "1:159663278849:web:5a70538d543c734e98e900",
+  measurementId: "G-0XCYKDBFDV"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
